0e3127e089857af31de2d60744ecd8ee984c79bc
Shubham <sm.phoujdar@gmail.com>
2021-03-27 07:59:41 +0530

    Refactored : Could not complete UC4 as use of optional not clear. Moving on to UC5.

--- src/main/java/GenericsPracticeProblem.java
+++ src/main/java/GenericsPracticeProblem.java
@@ -1,12 +1,34 @@
-public class GenericsPracticeProblem <T extends  Comparable<T>> {
+import java.util.Optional;
+
+public class GenericsPracticeProblem <T extends Comparable<T>> {
 
     T first,second,third;
+
     public GenericsPracticeProblem(T first, T second, T third) {
         this.first = first;
         this.second = second;
         this.third = third;
     }
+
+    public T compareFourWrapperObjects(){
+        T maxElement = this.first;
+        if (this.second.compareTo(maxElement) > 0)
+            maxElement = this.second;
+        if (this.third.compareTo(maxElement) > 0)
+            maxElement = this.third;
+        return maxElement;
+    }
+
     public T compareThreeWrapperObjects(){
+        T maxElement = this.first;
+        if (this.second.compareTo(maxElement) > 0)
+            maxElement = this.second;
+        if (this.third.compareTo(maxElement) > 0)
+            maxElement = this.third;
+        return maxElement;
+    }
+
+    public T printMax(){
         T maxElement = this.first;
         if (this.second.compareTo(maxElement) > 0)
             maxElement = this.second;
--- src/test/java/GenericsPracticeProblemTest.java
+++ src/test/java/GenericsPracticeProblemTest.java
@@ -1,57 +1,59 @@
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 
+import java.util.Optional;
+
 public class GenericsPracticeProblemTest {
     @Test
-    public void givenIntegers_MaxElementAsFirstElement_ShouldReturnMax(){
+    public void given3Integers_MaxElementAsFirstElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem(6,5,4);
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),6);
     }
 
     @Test
-    public void givenIntegers_MaxElementAsSecondElement_ShouldReturnMax(){
+    public void given3Integers_MaxElementAsSecondElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem(12,37,1);
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),37);
     }
 
     @Test
-    public void givenIntegers_MaxElementAsThirdElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8,1000,10001);
+    public void given3Integers_MaxElementAsThirdElement_ShouldReturnMax(){
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8,1000,1000);
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),10001);
     }
 
     @Test
-    public void givenFloat_MaxElementAsFirstElement_ShouldReturnMax(){
+    public void given3Float_MaxElementAsFirstElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem(66.23,5.05,0.445);
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),66.23);
     }
 
     @Test
-    public void givenFloat_MaxElementAsSecondElement_ShouldReturnMax(){
+    public void given3Float_MaxElementAsSecondElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem(122.32,371.71,1.00);
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),371.71);
     }
 
     @Test
-    public void givenFloat_MaxElementAsThirdElement_ShouldReturnMax(){
+    public void given3Float_MaxElementAsThirdElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8.66,10.00,77.0001);
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),77.0001);
     }
 
     @Test
-    public void givenString_MaxElementAsFirstElement_ShouldReturnMax(){
+    public void given3String_MaxElementAsFirstElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem("Zebra","Orangutan","Lemur");
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),"Zebra");
     }
 
     @Test
-    public void givenString_MaxElementAsSecondElement_ShouldReturnMax(){
+    public void given3String_MaxElementAsSecondElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem("Apple","Peach","Banana");
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),"Peach");
     }
 
     @Test
-    public void givenString_MaxElementAsThirdElement_ShouldReturnMax(){
+    public void given3String_MaxElementAsThirdElement_ShouldReturnMax(){
         GenericsPracticeProblem testVariable = new GenericsPracticeProblem("Apple","Bitcoin","Capgemini");
         Assert.assertEquals(testVariable.compareThreeWrapperObjects(),"Capgemini");
     }
