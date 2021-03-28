f55cf60945f6de1c7810068b44531ee48a65f474
Shubham <sm.phoujdar@gmail.com>
2021-03-28 08:05:05 +0530

    Added: UC4 methods using Options and Sorting

--- src/main/java/GenericsPracticeProblem.java
+++ src/main/java/GenericsPracticeProblem.java
@@ -1,22 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Optional;
 
 public class GenericsPracticeProblem <T extends Comparable<T>> {
 
     T first,second,third;
 
+    public GenericsPracticeProblem() {
+    }
+
     public GenericsPracticeProblem(T first, T second, T third) {
         this.first = first;
         this.second = second;
         this.third = third;
-    }
-
-    public T compareFourWrapperObjects(){
-        T maxElement = this.first;
-        if (this.second.compareTo(maxElement) > 0)
-            maxElement = this.second;
-        if (this.third.compareTo(maxElement) > 0)
-            maxElement = this.third;
-        return maxElement;
     }
 
     public T compareThreeWrapperObjects(){
@@ -25,11 +21,18 @@ public class GenericsPracticeProblem <T extends Comparable<T>> {
             maxElement = this.second;
         if (this.third.compareTo(maxElement) > 0)
             maxElement = this.third;
+        printMax(Optional.of((maxElement)));
+        return maxElement;
+    }
+
+    public Optional<T> compareArrayOfWrapperObjects(T ... parameter){
+        Optional<T> maxElement;
+        maxElement = Arrays.stream(parameter).sorted(Comparator.reverseOrder()).findFirst();
         printMax(maxElement);
         return maxElement;
     }
 
-    public void printMax(T maxElement){
-        System.out.println("Max Element is: "+ maxElement);
+    public void printMax(Optional<T> maxElement){
+        System.out.println("Max Element is: "+ maxElement.get());
     }
 }
--- src/test/java/GenericsPracticeProblemTest.java
+++ src/test/java/GenericsPracticeProblemTest.java
@@ -1,60 +1,87 @@
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
-
 import java.util.Optional;
 
 public class GenericsPracticeProblemTest {
     @Test
-    public void given3Integers_MaxElementAsFirstElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(6,5,4);
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),6);
+    public void given3Integers_MaxElementAsFirstElement_ShouldReturnMax() {
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(6, 5, 4);
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), 6);
     }
 
     @Test
-    public void given3Integers_MaxElementAsSecondElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(12,37,1);
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),37);
+    public void given3Integers_MaxElementAsSecondElement_ShouldReturnMax() {
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(12, 37, 1);
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), 37);
     }
 
     @Test
-    public void given3Integers_MaxElementAsThirdElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8,1000,10001);
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),10001);
+    public void given3Integers_MaxElementAsThirdElement_ShouldReturnMax() {
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8, 1000, 10001);
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), 10001);
     }
 
     @Test
-    public void given3Float_MaxElementAsFirstElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(66.23,5.05,0.445);
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),66.23);
+    public void given3Float_MaxElementAsFirstElement_ShouldReturnMax() {
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(66.23F, 5.05F, 0.445F);
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), 66.23F);
     }
 
     @Test
-    public void given3Float_MaxElementAsSecondElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(122.32,371.71,1.00);
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),371.71);
+    public void given3Float_MaxElementAsSecondElement_ShouldReturnMax() {
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(122.32F, 371.71F, 1.00F);
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), 371.71F);
     }
 
     @Test
-    public void given3Float_MaxElementAsThirdElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8.66,10.00,77.0001);
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),77.0001);
+    public void given3Float_MaxElementAsThirdElement_ShouldReturnMax() {
+        GenericsPracticeProblem testVariable = new GenericsPracticeProblem(8.66F, 10.00F, 77.0001F);
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), 77.0001F);
     }
 
     @Test
-    public void given3String_MaxElementAsFirstElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem("Zebra","Orangutan","Lemur");
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),"Zebra");
+    public void given3String_MaxElementAsFirstElement_ShouldReturnMax() {
+        GenericsPracticeProblem<String> testVariable = new GenericsPracticeProblem<String>("Zebra", "Orangutan", "Lemur");
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), "Zebra");
     }
 
     @Test
-    public void given3String_MaxElementAsSecondElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem("Apple","Peach","Banana");
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),"Peach");
+    public void given3String_MaxElementAsSecondElement_ShouldReturnMax() {
+        GenericsPracticeProblem<String> testVariable = new GenericsPracticeProblem<String>("Apple", "Peach", "Banana");
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), "Peach");
     }
 
     @Test
-    public void given3String_MaxElementAsThirdElement_ShouldReturnMax(){
-        GenericsPracticeProblem testVariable = new GenericsPracticeProblem("Apple","Bitcoin","Capgemini");
-        Assert.assertEquals(testVariable.compareThreeWrapperObjects(),"Capgemini");
+    public void given3String_MaxElementAsThirdElement_ShouldReturnMax() {
+        GenericsPracticeProblem<String> testVariable = new GenericsPracticeProblem<String>("Apple", "Bitcoin", "Capgemini");
+        Assert.assertEquals(testVariable.compareThreeWrapperObjects(), "Capgemini");
+    }
+
+    @Test
+    public void givenStringArray_MaxElementAsLastElement_ShouldReturnMax() {
+        GenericsPracticeProblem<String> testVariable = new GenericsPracticeProblem<String>();
+        String[] compareElementsArray = {"Aa", "Jo", "Root", "Reema", "Volvo"};
+        Assert.assertEquals(testVariable.compareArrayOfWrapperObjects(compareElementsArray), Optional.of("Volvo"));
+    }
+
+    @Test
+    public void givenIntegerArray_MaxElementAsLastElement_ShouldReturnMax() {
+        GenericsPracticeProblem<Integer> testVariable = new GenericsPracticeProblem<Integer>();
+        Integer[] compareElementsArray = {88, 0, 661, 55, 3, 4};
+        Assert.assertEquals(testVariable.compareArrayOfWrapperObjects(compareElementsArray), Optional.of(661));
+    }
+
+    @Test
+    public void givenDoubleArray_MaxElementAsLastElement_ShouldReturnMax() {
+        GenericsPracticeProblem<Double> testVariable = new GenericsPracticeProblem<Double>();
+        Double[] compareElementsArray = {98.22, 6.15, 0.32};
+        Assert.assertEquals(testVariable.compareArrayOfWrapperObjects(compareElementsArray), Optional.of(98.22));
+    }
+
+    @Test
+    public void givenCharacterArray_MaxElementAsLastElement_ShouldReturnMax() {
+        GenericsPracticeProblem<Character> testVariable = new GenericsPracticeProblem<Character>();
+        Character[] compareElementsArray = {'b', 'h', 'a', 'm', 's', 'h', 'y', 'a'};
+        Assert.assertEquals(testVariable.compareArrayOfWrapperObjects(compareElementsArray), Optional.of('y'));
     }
 }
